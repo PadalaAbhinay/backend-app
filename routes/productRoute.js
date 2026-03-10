2e405c7b67759ca077dd3eb417c0abb043161c33
PadalaAbhinay <padalaabhinay5@gmail.com>
2026-03-10 12:33:55 +0530

    commit

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,6 +1,12 @@
-import {getProducts} from "../controllers/productController.js";
+import {getProducts,addProductForm,addProduct, deleteProduct, editProductform, saveProduct} from "../controllers/productController.js";
 import express from "express";
 const productRouter = express.Router();
+
 productRouter.get("/", getProducts);
+productRouter.get("/add",addProductForm);
+productRouter.post("/add",addProduct)
+productRouter.get("/:id/delete",deleteProduct);
+productRouter.get("/:id/edit",editProductform);
+productRouter.post("/:id/save",saveProduct);
 
 export {productRouter}
